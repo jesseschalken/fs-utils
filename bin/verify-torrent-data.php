dc41f923acdc054e52db5997383f3cea3b015cb1
Jesse Schalken <me@jesseschalken.com>
2014-08-17 19:14:41 +1000

    Yet more work on bin/verify-torrent-data.php

--- bin/verify-torrent-data.php
+++ bin/verify-torrent-data.php
@@ -2,8 +2,11 @@
 
 namespace PureBencode;
 
+use Traversable;
+
 error_reporting(-1);
 ini_set('display_errors', 'On');
+mb_internal_encoding('UTF-8');
 
 const DIR_SEP = DIRECTORY_SEPARATOR;
 
@@ -12,40 +15,130 @@ require_once __DIR__ . '/../vendor/autoload.php';
 function shorten($string) {
     $string = json_encode($string, JSON_UNESCAPED_UNICODE);
     $maxlen = 60;
-    if (strlen($string) <= $maxlen)
+    if (mb_strlen($string) <= $maxlen)
         return $string;
-    $part1 = substr($string, 0, $maxlen / 2);
-    $part2 = substr($string, -$maxlen / 2);
+    $part1 = mb_substr($string, 0, $maxlen / 2);
+    $part2 = mb_substr($string, -$maxlen / 2);
     return "$part1...$part2";
+}
+
+function readFiles(array $files, $length = 1024) {
+    foreach ($files as $file) {
+        $f = fopen($file, 'rb');
+        while (!feof($f))
+            yield fread($f, $length);
+        fclose($f);
+    }
+}
+
+function chunk(Traversable $data, $length) {
+    $buffer = '';
+    foreach ($data as $piece) {
+        $buffer .= $piece;
+        while (strlen($buffer) > $length) {
+            yield substr($buffer, 0, $length);
+            $buffer = substr($buffer, $length);
+        }
+    }
+    if (strlen($buffer) > 0)
+        yield $buffer;
+}
+
+function formatBytes($bytes) {
+    $f = array('', 'K', 'M', 'G', 'T', 'P', 'E', 'Z', 'Y');
+    $i = (int)floor(log(max(abs($bytes), 1), 1000));
+    return number_format($bytes / pow(1000, $i), 2) . $f[$i] . 'B';
 }
 
 class VerifyTorrentData {
     static function run() {
         global $argv;
 
-        $self = new self($argv[1], array_slice($argv, 2));
-        $self->_run();
+        $self          = new self;
+        $self->dataDir = $argv[1];
+
+        foreach (array_slice($argv, 2) as $name) {
+            $self->out("reading " . shorten($name));
+            $torrent          = TorrentInfo::parse($name);
+            $self->torrents[] = $torrent;
+            $self->totalSize += $torrent->totalSize();
+        }
+
+        for ($self->i = 0; $self->i < count($self->torrents); $self->i++)
+            $self->doTorrent();
     }
 
     /** @var string */
-    private $dir;
-    /** @var string[] */
+    private $dataDir;
+    /** @var TorrentInfo[] */
     private $torrents;
+    private $i;
+    private $totalSize = 0.0;
+    private $sizeDone = 0.0;
 
-    function __construct($dir, array $torrents) {
-        $this->dir      = $dir;
-        $this->torrents = $torrents;
+    private function doTorrent() {
+        $info        = $this->torrents[$this->i];
+        $files       = array();
+        $oldSizeDone = $this->sizeDone;
+
+        $this->out("checking files");
+        foreach ($info->files as $file => $size) {
+            $path = $this->dataDir . DIR_SEP . $file;
+
+            if (!file_exists($path)) {
+                $this->out("file $path does not exist\n");
+            } else if (!is_readable($path)) {
+                $this->out("file $path is not readable\n");
+            } else if (!is_file($path)) {
+                $this->out("file $path is not a file\n");
+            } else {
+                $filesize = filesize($path);
+                if ($filesize != $size) {
+                    $this->out("$path file size mismatch. expected $size bytes, got $filesize bytes\n");
+                } else {
+                    $files[] = $path;
+                }
+            }
+        }
+
+        if (count($files) == count($info->files)) {
+            $count = count($info->pieces);
+            $this->out("verifying data");
+            foreach (chunk(readFiles($files), $info->pieceSize) as $i => $piece) {
+                $this->sizeDone += strlen($piece);
+                $hash     =& $info->pieces[$i];
+                $filehash = sha1($piece, true);
+
+                if (!$hash) {
+                    $this->out("piece $i onward is missing from torrent file\n");
+                    break;
+                } else if ($filehash !== $hash) {
+                    $this->out("piece $i/$count hash mismatch, expected " . bin2hex($hash) . ", got " . bin2hex($filehash) . "\n");
+                    break;
+                } else {
+                    $this->out("verifying data");
+                }
+            }
+        }
+
+        $this->sizeDone = $oldSizeDone + $info->totalSize();
     }
 
-    private function _run() {
-        $i     = 1;
-        $count = count($this->torrents);
-        foreach ($this->torrents as $torrent) {
-            $string = shorten($torrent);
-            $verify = new TorrentVerify(TorrentInfo::parse($torrent), "[$i/$count] $string", $this->dir);
-            $verify->run();
-            $i++;
+    private function out($string = '') {
+        $percent      = number_format($this->sizeDone * 100 / max($this->totalSize, 1), 2) . '%';
+        $bytesDone    = formatBytes($this->sizeDone) . ' of ' . formatBytes($this->totalSize);
+        $torrentsDone = ($this->i + 1) . '/' . count($this->torrents);
+
+        print "\r\x1B[2K";
+        print "[$percent, $bytesDone, $torrentsDone]";
+
+        if ($this->i !== null) {
+            $info = $this->torrents[$this->i];
+            print " " . shorten($info->filename);
         }
+
+        if ($string)
+            print ": $string";
     }
 }
 
@@ -70,170 +163,25 @@ class TorrentInfo {
 
         $self->pieceSize = $info['piece length'];
         $self->pieces    = str_split($info['pieces'], 20);
+        $self->filename  = $torrent;
 
         return $self;
     }
 
+    /** @var string */
+    public $filename;
     /** @var int[] */
-    private $files = array();
+    public $files = array();
     /** @var string[] */
-    private $pieces = array();
+    public $pieces = array();
     /** @var int */
-    private $pieceSize = 0;
+    public $pieceSize = 0;
 
-    function files() {
-        return array_keys($this->files);
-    }
-
-    function fileSize($file) {
-        return $this->files[$file];
-    }
-
-    function numPieces() {
-        return count($this->pieces);
-    }
-
-    function hasPiece($i) {
-        return isset($this->pieces[$i]);
-    }
-
-    function piece($i) {
-        return $this->pieces[$i];
-    }
-
-    function numFilePieces() {
-        $total = 0;
-        foreach ($this->files as $size)
-            $total += $size / $this->pieceSize;
-        return (int)ceil($total);
-    }
-
-    function numFiles() {
-        return count($this->files);
-    }
-
-    function pieceSize() {
-        return $this->pieceSize;
-    }
-}
-
-class TorrentVerify {
-    /** @var TorrentInfo */
-    private $torrent;
-    private $buffer = '';
-    private $fileIndex = 0;
-    private $pieceIndex = 0;
-    private $currentFile;
-    private $prefix;
-    private $baseDir;
-    private $continue = true;
-
-    function __construct(TorrentInfo $torrent, $prefix, $baseDir) {
-        $this->torrent = $torrent;
-        $this->prefix  = $prefix;
-        $this->baseDir = $baseDir;
-    }
-
-    function run() {
-        foreach ($this->torrent->files() as $file) {
-            $this->currentFile = $file;
-
-            if ($this->continue)
-                $this->checkFile();
-
-            if ($this->continue)
-                $this->processFile();
-
-            $this->fileIndex++;
-        }
-
-        if ($this->continue)
-            $this->runBuffer(true);
-    }
-
-    private function printProgress() {
-        $numPieces  = $this->torrent->numPieces();
-        $numFiles   = $this->torrent->numFiles();
-        $percent    = number_format($this->pieceIndex * 100 / $numPieces, 2);
-        $pieceIndex = $this->pieceIndex + 1;
-        $fileIndex  = $this->fileIndex + 1;
-        $file       = shorten($this->currentFile);
-        $this->out("[$percent%, piece $pieceIndex/$numPieces, file $fileIndex/$numFiles] $file");
-    }
-
-    private function checkFile() {
-        $path = $this->filePath();
-
-        if (!file_exists($path)) {
-            $this->out("file $path does not exist\n");
-            $this->continue = false;
-        } else if (!is_readable($path)) {
-            $this->out("file $path is not readable\n");
-            $this->continue = false;
-        } else if (!is_file($path)) {
-            $this->out("file $path is not a file\n");
-            $this->continue = false;
-        } else {
-            $filesize = filesize($path);
-            $size     = $this->torrent->fileSize($this->currentFile);
-            if ($filesize != $size) {
-                $this->out("$path file size mismatch. expected $size bytes, got $filesize bytes\n");
-                $this->continue = false;
-            }
-        }
-    }
-
-    private function runBuffer($last) {
-        $pieceSize = $this->torrent->pieceSize();
-        $numPieces = $this->torrent->numPieces();
-
-        while (($last && $this->buflen() > 0) || $this->buflen() >= $pieceSize) {
-            if (!$this->continue)
-                break;
-            $this->printProgress();
-            $piece        = (string)substr($this->buffer, 0, $pieceSize);
-            $this->buffer = (string)substr($this->buffer, strlen($piece));
-            $pieceIndex   = $this->pieceIndex + 1;
-
-            if (!$this->torrent->hasPiece($this->pieceIndex)) {
-                $this->out("piece $pieceIndex/$numPieces is missing\n");
-            } else {
-                $hash  = $this->torrent->piece($this->pieceIndex);
-                $hash2 = sha1($piece, true);
-
-                if ($hash !== $hash2) {
-                    $this->out("piece $pieceIndex/$numPieces: hash mismatch, expected " . bin2hex($hash) . ", got " . bin2hex($hash2) . "\n");
-                    $this->continue = false;
-                }
-            }
-
-            $this->pieceIndex++;
-        }
-    }
-
-    private function out($string = '') {
-        print "\r\x1B[2K";
-        print $this->prefix;
-
-        if ($string)
-            print " $string";
-    }
-
-    private function filePath() {
-        return $this->baseDir . DIR_SEP . $this->currentFile;
-    }
-
-    private function buflen() {
-        return strlen($this->buffer);
-    }
-
-    private function processFile() {
-        $f = fopen($this->filePath(), 'rb');
-        while (!feof($f) && $this->continue) {
-            $this->buffer .= fread($f, $this->torrent->pieceSize());
-            $this->runBuffer(false);
-        }
-        fclose($f);
+    function totalSize() {
+        $size = 0.0;
+        foreach ($this->files as $size1)
+            $size += $size1;
+        return $size;
     }
 }
 
