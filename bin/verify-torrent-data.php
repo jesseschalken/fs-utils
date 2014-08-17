00ea4388e95aaf56cbc5e177ae88484aff939540
Jesse Schalken <me@jesseschalken.com>
2014-08-17 14:23:45 +1000

    Work on bin/verify-torrent-data.php

--- bin/verify-torrent-data.php
+++ bin/verify-torrent-data.php
@@ -11,8 +11,11 @@ array_shift($argv);
 $dir = array_shift($argv);
 print "dir: $dir\n";
 
+$clear = "\r\x1B[2K";
+
+$j = 1;
+$total = count($argv);
 foreach ($argv as $torrent) {
-    print "torrent: $torrent\n";
     $info = Bencode::decode(file_get_contents($torrent))['info'];
 
     if (isset($info['name']))
@@ -30,50 +33,63 @@ foreach ($argv as $torrent) {
     $pieceSize = $info['piece length'];
     $pieces = str_split($info['pieces'], 20);
     $buffer = '';
-    $i = 0;
+    $total2 = count($files);
+    $totalPieces = 0;
+    foreach ($files as $size)
+        $totalPieces += $size / $pieceSize;
+    $totalPieces = ceil($totalPieces);
+    $k = 1;
+    $i = 1;
     foreach ($files as $name => $size) {
-        print "  $name\n";
         $path = "$dir/$name";
 
+        print $clear;
+
         if (!file_exists($path)) {
-            print "file $path does not exist\n";
+            print "{$clear}file $path does not exist\n";
             continue;
         }
 
         if (!is_readable($path)) {
-            print "file $path is not readable\n";
+            print "{$clear}file $path is not readable\n";
             continue;
         }
 
         if (!is_file($path)) {
-            print "$path is not a file\n";
+            print "{$clear}$path is not a file\n";
             continue;
         }
 
         $filesize = filesize($path);
 
         if ($filesize != $size) {
-            print "file size mismatch. expected $size bytes, got $filesize bytes\n";
+            print "\nfile size mismatch. expected $size bytes, got $filesize bytes\n";
         }
 
         $f = fopen($path, 'r');
         while (!feof($f)) {
             $buffer .= fread($f, $pieceSize);
             while (strlen($buffer) >= $pieceSize) {
+                $percent = number_format($i*100/$totalPieces,2);
+                print "{$clear}$j/$total, $torrent: $percent% [$i/$totalPieces] $k/$total2, $path";
                 $piece = substr($buffer, 0, $pieceSize);
                 $buffer = substr($buffer, $pieceSize);
                 $hash = array_shift($pieces);
                 if (!is_string($hash)) {
-                    print "torrent is missing hash for peice #$i\n";
+                    print "{$clear}$torrent: piece $i/$totalPieces missing\n";
                     continue;
                 }
                 $hash2 = sha1($piece, true);
                 if ($hash !== $hash2) {
-                    print "hash mismatch at piece #$i: expected $hash, got $hash2\n";
+                    print "{$clear}$torrent: piece $i/$totalPieces: hash mismatch, expected " . bin2hex($hash) . ", got " . bin2hex($hash2) . "\n";
                 }
                 $i++;
             }
         }
         fclose($f);
+        $k++;
     }
+    $j++;
 }
+
+print "\n";
