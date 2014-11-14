0f1c915983167372b93978570a7cc15c1ef02e62
Jesse Schalken <me@jesseschalken.com>
2014-11-14 14:12:58 +1100

    Refactoring. Collapsed AbstractFile hierarchy.

--- file.php
+++ file.php
@@ -13,181 +13,93 @@ function hash($s) {
     return hash_final($h);
 }
 
-function read_file($path) {
-    $f = fopen($path, 'rb');
-    while ($f && !feof($f))
-        yield fread($f, 10000000);
-    fclose($f);
-}
-
-abstract class AbstractFile {
-    /**
-     * @param string    $path
-     * @param Directory $parent
-     * @param callable  $callback
-     * @throws \Exception
-     * @return self
-     */
-    static function create($path, Directory $parent = null, callable $callback = null) {
-        $fullPath = $parent ? $parent->concat($path) : $path;
-        if ($callback)
-            $callback($fullPath);
-        $type = filetype($fullPath);
-        switch ($type) {
-            case 'fifo':
-                return new NamedPipe($path, $parent);
-            case 'char':
-                return new CharacterDevice($path, $parent);
-            case 'dir':
-                return new Directory($path, $parent, $callback);
-            case 'block':
-                return new BlockDevice($path, $parent);
-            case 'link':
-                return new SymbolicLink($path, $parent);
-            case 'file':
-                return new RegularFile($path, $parent);
-            case 'socket':
-                return new Socket($path, $parent);
-            case 'unknown':
-                return new Unknown($path, $parent);
-            default:
-                throw new \Exception("unknown file type: $type");
-        }
-    }
+class File {
+    const FIFO    = 'fifo';
+    const CHAR    = 'char';
+    const DIR     = 'dir';
+    const BLOCK   = 'block';
+    const LINK    = 'link';
+    const FILE    = 'file';
+    const SOCKET  = 'socket';
+    const UNKNOWN = 'unknown';
 
     /** @var string */
     private $path;
-    /** @var Directory|null */
-    private $parent;
 
-    function __construct($path, Directory $parent = null) {
-        $this->path   = $path;
-        $this->parent = $parent;
+    function __construct($path) {
+        $this->path = $path;
     }
 
-    function fullPath() {
-        $path   = $this->path;
-        $parent = $this->parent;
-        return $parent ? $parent->concat($path) : $path;
+    function path() { return $this->path; }
+
+    /**
+     * @param Progress $progress
+     * @param Hashes   $hashes
+     * @return \Generator
+     */
+    function contents(Progress $progress, Hashes $hashes) {
+        switch ($this->type()) {
+            case self::DIR:
+                /** @var File $child */
+                foreach ($this->scanDir() as $name) {
+                    $hash = $hashes->add($this->join($name), $progress);
+                    yield "$hash $name\n";
+                }
+                break;
+            case self::LINK:
+                yield readlink($this->path);
+                break;
+            case self::FILE:
+                foreach ($progress->thread($this->readFile(), $this->path) as $s)
+                    yield $s;
+        }
     }
 
-    function contents(Progress $progress, Hashes $hashes) { yield ''; }
+    function size(\Closure $f) {
+        $f($this->path);
+        switch ($this->type()) {
+            case self::FILE:
+                return filesize($this->path);
+            case self::DIR:
+                $size = 0;
+                foreach ($this->scanDir() as $s)
+                    $size += $this->join($s)->size($f);
+                return $size;
+            default:
+                return 0;
+        }
+    }
 
-    function count() { return 1; }
+    /**
+     * @return \Generator
+     */
+    function readFile() {
+        $f = fopen($this->path, 'rb');
+        while (!feof($f))
+            yield fread($f, 10000000);
+        fclose($f);
+    }
 
-    function size() { return 0; }
+    function name() { return pathinfo($this->path, PATHINFO_BASENAME); }
 
-    function name() { return $this->path; }
+    function scanDir() {
+        $files = scandir($this->path) ?: array();
+        $files = array_diff($files, ['.', '..']);
+        return $files;
+    }
 
-    function flatten() { yield $this; }
-
-    function describe() { return "({$this->type()}) {$this->fullPath()}"; }
-
-    abstract function type();
+    function type() {
+        return filetype($this->path);
+    }
 
     function delete() {
-        $cmd = "rm -rf " . escapeshellarg($this->fullPath());
+        $cmd = "rm -rf " . escapeshellarg($this->path);
         printReplace("$cmd\n");
         shell_exec($cmd);
     }
-}
 
-class Directory extends AbstractFile {
-    /** @var AbstractFile[] */
-    private $children = [];
-    private $size = 0;
-    private $count = 1;
-
-    function __construct($path, Directory $parent = null, callable $callback = null) {
-        parent::__construct($path, $parent);
-
-        $scan = scandir($this->fullPath());
-        $scan = array_diff($scan, ['.', '..']);
-        foreach ($scan as $s) {
-            $file = AbstractFile::create($s, $this, $callback);
-            $this->size += $file->size();
-            $this->count += $file->count();
-            $this->children[] = $file;
-        }
+    function join($file) {
+        return new self($this->path . DIRECTORY_SEPARATOR . $file);
     }
-
-    function concat($path) {
-        return $this->fullPath() . DIRECTORY_SEPARATOR . $path;
-    }
-
-    function contents(Progress $progress, Hashes $hashes) {
-        foreach ($this->children as $child) {
-            $hash = $hashes->add($child, $progress);
-            $name = $child->name();
-            yield "$hash $name\n";
-        }
-    }
-
-    function flatten() {
-        yield $this;
-
-        foreach ($this->children as $child)
-            foreach ($child->flatten() as $file)
-                yield $file;
-    }
-
-    function count() { return $this->count; }
-
-    function size() { return $this->size; }
-
-    function type() { return 'directory'; }
 }
 
-class RegularFile extends AbstractFile {
-    private $size;
-
-    function __construct($path, Directory $parent = null) {
-        parent::__construct($path, $parent);
-        $this->size = filesize($this->fullPath());
-    }
-
-    function contents(Progress $progress, Hashes $hashes) {
-        $path = $this->fullPath();
-        return $progress->thread(read_file($path), $path);
-    }
-
-    function size() { return $this->size; }
-
-    function type() { return 'regular file'; }
-}
-
-class SymbolicLink extends AbstractFile {
-    private $dest;
-
-    function __construct($path, Directory $parent = null) {
-        parent::__construct($path, $parent);
-
-        $this->dest = readlink($this->fullPath());
-    }
-
-    function contents(Progress $progress, Hashes $hashes) {
-        yield $this->dest;
-    }
-
-    function type() { return 'symbolic link'; }
-}
-
-class NamedPipe extends AbstractFile {
-    function type() { return 'named pipe'; }
-}
-
-class Socket extends AbstractFile {
-    function type() { return 'socket'; }
-}
-
-class BlockDevice extends AbstractFile {
-    function type() { return 'block device'; }
-}
-
-class CharacterDevice extends AbstractFile {
-    function type() { return 'character device'; }
-}
-
-class Unknown extends AbstractFile {
-    function type() { return 'unknown'; }
-}
--- main.php
+++ main.php
@@ -5,17 +5,6 @@ namespace FindDuplicateFiles;
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-/**
- * @param string $path
- * @return \Generator
- */
-function readFile($path) {
-    $f = fopen($path, 'rb');
-    while (!feof($f))
-        yield fread($f, 102400);
-    fclose($f);
-}
-
 function formatBytes($bytes) {
     $f = ['', 'K', 'M', 'G', 'T', 'P', 'E', 'Z', 'Y'];
     $i = (int)floor(log(max(abs($bytes), 1), 1000));
@@ -89,6 +78,11 @@ class Progress {
         printReplace($line);
     }
 
+    /**
+     * @param \Traversable $bytes
+     * @param string       $path
+     * @return \Generator
+     */
     function thread(\Traversable $bytes, $path) {
         $this->printProgress($path);
         foreach ($bytes as $data) {
@@ -101,10 +95,10 @@ class Progress {
 }
 
 class Hashes {
-    /** @var AbstractFile[][] */
+    /** @var File[][] */
     private $hashes = [];
 
-    function add(AbstractFile $file, Progress $progress) {
+    function add(File $file, Progress $progress) {
         $hash = hash($file->contents($progress, $this));
         $hash = "[{$file->type()}] $hash";
 
@@ -114,7 +108,7 @@ class Hashes {
 
     /**
      * @param string $hash
-     * @return AbstractFile[]
+     * @return File[]
      */
     function files($hash) {
         return $this->hashes[$hash];
@@ -123,7 +117,8 @@ class Hashes {
     function sorted() {
         $sizes = [];
         foreach ($this->hashes as $hash => $files)
-            $sizes[$hash] = $this->amountDuplicated($hash);
+            if (count($files) > 0)
+                $sizes[$hash] = $this->amountDuplicated($hash);
         arsort($sizes, SORT_NUMERIC);
         return array_keys($sizes);
     }
@@ -132,66 +127,10 @@ class Hashes {
         $size  = 0;
         $files = $this->files($hash);
         foreach ($files as $file)
-            $size += $file->size();
+            $size += $file->size(function () { });
         $size -= $size / count($files);
         return $size;
     }
-}
-
-const DIR_SEP = DIRECTORY_SEPARATOR;
-
-/**
- * @param string $path
- * @param int[]  $sizes
- * @return int
- */
-function getSize($path, array &$sizes) {
-    printReplace("calculating size: $path");
-    $size = 0;
-    if (is_dir($path) && !is_link($path)) {
-        $scan = scandir($path);
-        $scan = array_diff($scan, ['.', '..']);
-        foreach ($scan as $s)
-            $size += getSize($path . DIR_SEP . $s, $sizes);
-    } else if (is_file($path)) {
-        $size += filesize($path);
-    }
-    $sizes[$path] = $size;
-    return $size;
-}
-
-/**
- * @param string   $path
- * @param (string[])[]    $hashes
- * @param Progress $p
- * @return null|string
- */
-function getHash($path, array &$hashes, Progress $p) {
-    $h = hash_init('sha1');
-
-    if (is_dir($path) && !is_link($path)) {
-        hash_update($h, "dir\n");
-        $scan = scandir($path);
-        $scan = array_diff($scan, ['.', '..']);
-        foreach ($scan as $s) {
-            $hash2 = getHash($path . DIR_SEP . $s, $hashes, $p) ? : str_repeat("\x00", 20);
-            hash_update($h, "$hash2 $s\n");
-        }
-    } else if (is_file($path)) {
-        foreach (readFile($path) as $piece) {
-            $p->add(strlen($piece));
-            hash_update($h, $piece);
-            $p->printProgress($path);
-        }
-    } else {
-        return null;
-    }
-
-    $hash = hash_final($h, true);
-
-    $hashes[$hash][] = $path;
-
-    return $hash;
 }
 
 function runReport(Hashes $hashes, $limit = null) {
@@ -212,7 +151,7 @@ function runReport(Hashes $hashes, $limit = null) {
 
         $options = [];
         foreach ($files as $k => $file)
-            $options[$k + 1] = "Keep only \"{$file->fullPath()}\"";
+            $options[$k + 1] = "Keep only \"{$file->path()}\"";
         $options['s'] = 'Skip this duplicate';
         $options['q'] = 'Quit';
 
@@ -262,21 +201,19 @@ s
     $paths = $args['<path>'];
     $limit = $args['--limit'];
 
-    /** @var AbstractFile[] $files */
+    /** @var File[] $files */
     $files = [];
     $size  = 0;
-    $count = 0;
     foreach ($paths as $path) {
-        $file = AbstractFile::create($path, null, function ($path) {
-            printReplace("scanning $path...");
+        $file = new File($path);
+        $size += $file->size(function ($path) {
+            printReplace("scanning $path");
         });
-        $count += $file->count();
-        $size += $file->size();
         $files[] = $file;
     }
     printReplace();
 
-    print "$count files, " . formatBytes($size) . "\n";
+    print "found " . formatBytes($size) . "\n";
 
     $progress = new Progress($size);
     $hashes   = new Hashes;
